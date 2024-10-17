455ee817de93b853eacaf60d748e200539a4f4b5
kishan96-coder <kishansingh150127@gmail.com>
2024-10-18 00:11:04 +0530

    18 october homeowork devtinder postman

---
+++ homewrok.md
@@ -0,0 +1,9 @@
+18-October
+
+- Initialize git
+- gitignore
+- Create a remote repo on github
+- Push all code to remote origin
+- Play with routes and route extensions ex. /hello, / ,hello/2, /xyz
+- Order of the routes matter a lot
+- Install Postman app and make a workspace/collection test API call
--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
+
+
 app.use('/test',(req,res)=>{
     res.send('Hello testing testing')
+});
+
+app.use('/hello/2',(req,res)=>{
+    res.send('Hello my second bro')
 });
 
 app.use('/hello',(req,res)=>{
     res.send('Hello to express js')
 });
 
-app.listen((3333));
+app.use('/',(req,res)=>{
+    res.send('Hello server')
+});
+app.listen((3333),()=>{
+    console.log("Connect to server");
+});
 
